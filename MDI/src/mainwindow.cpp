658e89df16fbb1d09035ff8f4fafa876b9c90ea0
anycad <anycad@anycad.net>
2013-02-20 14:44:44 +0800

    Add ToolBox Add QSS

--- MDI/src/mainwindow.cpp
+++ MDI/src/mainwindow.cpp
@@ -1,42 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2012 Digia Plc and/or its subsidiary(-ies).
-** Contact: http://www.qt-project.org/legal
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names
-**     of its contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
 #include <QtWidgets>
 
@@ -45,10 +6,14 @@
 
 MainWindow::MainWindow()
 {
-    mdiArea = new QMdiArea;
+    mdiArea = new QMdiArea();
+    mdiArea->setBackground(QBrush(QColor(70,70,70)));
     mdiArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     mdiArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     mdiArea->setViewMode(QMdiArea::TabbedView);
+    mdiArea->setTabShape(QTabWidget::Triangular);
+    mdiArea->setTabsClosable(true);
+    mdiArea->setTabsMovable(true);
 
     setCentralWidget(mdiArea);
     connect(mdiArea, SIGNAL(subWindowActivated(QMdiSubWindow*)),
@@ -420,19 +385,33 @@ void MainWindow::createDockWindow()
    {
       QDockWidget *dock = new QDockWidget(tr("Browser"), this);
       dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
-      dockingList = new QListWidget(dock);
-      dockingList->addItems(QStringList()
+      dockingList = new QTreeWidget(dock);
+      dock->setWidget(dockingList);
+      addDockWidget(Qt::LeftDockWidgetArea, dock);
+      editMenu->addAction(dock->toggleViewAction());
+   }
+   {
+      QDockWidget *dock = new QDockWidget(tr("ToolBox"), this);
+      dock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+      toolBox = new QToolBox(dock);
+
+      QListWidget* mdockingList = new QListWidget(dock);
+      mdockingList->addItems(QStringList()
          << "1"
          << "2"
          << "3"
          << "4"
          << "5"
          << "6");
-      dock->setWidget(dockingList);
-      addDockWidget(Qt::LeftDockWidgetArea, dock);
+
+      toolBox->addItem(mdockingList, "Test");
+      toolBox->addItem(new QPushButton("B"), "Test2");
+      toolBox->addItem(new QPushButton("C"), "Test3");
+      dock->setWidget(toolBox);
+
+      addDockWidget(Qt::RightDockWidgetArea, dock);
       editMenu->addAction(dock->toggleViewAction());
    }
-
    {
       QDockWidget *dock = new QDockWidget(tr("Output"), this);
       dock->setAllowedAreas(Qt::AllDockWidgetAreas);
